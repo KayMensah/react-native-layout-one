f341c1fbf2bda809cb1d2213f1d01c44072cd37c
Korkor Mensah <komensah2018@gmail.com>
2022-05-16 08:36:43 +0000

    react-native-layout-one

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import LayoutOne from "./src/LayoutOne";
+
+const App = () => {
+  return <LayoutOne />;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
